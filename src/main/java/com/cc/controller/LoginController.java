134148369285e676c8bf716515556012ac0c71f6
bigcong <cuicong0707@163.com>
2015-07-03 19:12:58 +0800

    修改方法

--- src/main/java/com/cc/controller/LoginController.java
+++ src/main/java/com/cc/controller/LoginController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +30,7 @@ public class LoginController {
      * @author: cuicong
      */
     @RequestMapping("login")
-    public User login(HttpServletRequest reques, @RequestBody User user) {
+    public User login(HttpServletRequest reques, User user) {
         User u = this.loginService.getUser(user.getUserName(), user.getPassword());
         // 将用户名存到session当中
         reques.getSession().setAttribute("user", u);
